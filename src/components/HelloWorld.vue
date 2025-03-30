155e7ff28b2c723982a08afb9e55a5ab222b7b9e
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-29 23:01:49 -0600

    Agregando un nuevo parametro

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,14 @@
 <script setup>
 import { ref } from 'vue'
 
-// defineProps({
-//   msg: String,
-// })
-
+defineProps({
+  nombre: String,
+})
 </script>
 
 <template>
- 
+ <h1>{{ nombre }}</h1>
 </template>
 
-<style scoped>
+<style scoped></style>
 
-</style>
