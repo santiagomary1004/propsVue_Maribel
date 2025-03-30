3bf2d76618e0527faeacde647b3e2a346609e582
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-29 23:18:16 -0600

    Modificando componentes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Padre from './components/Props/Padre.vue';
 </script>
 
 <template>
-  <HelloWorld nombre="Maribel Santiago" />
+  <!-- <HelloWorld nombre="Maribel Santiago" /> -->
+   <Padre/>
 </template>
 
 <style scoped></style>
